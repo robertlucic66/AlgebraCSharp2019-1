ff6f1cf6a936df9dcddd2bf15f7a885a96e36f50
Robert Lucić <robertlucic66@outlook.com>
2019-02-25 17:22:03 +0100

    switch primjer

---
+++ ConsoleApp1/broj_veci_od10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace broj_veci_od10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite prirodan broj: ");
+            int a = int.Parse(Console.ReadLine());
+            if (a > 10)
+            {
+                Console.WriteLine("Uneseni broj je veći od 10.");
+            }
+            else
+            {
+                Console.WriteLine("Uneseni broj NIJE veći od 10.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
