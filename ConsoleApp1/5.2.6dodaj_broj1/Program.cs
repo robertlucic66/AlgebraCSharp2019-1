45679acf348c1d2558accbb74c943688fd458b0f
Robert Lucić <robertlucic66@outlook.com>
2019-02-26 16:47:03 +0100

    dodaj broj 2

---
+++ ConsoleApp1/5.2.6dodaj_broj1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2._6dodaj_broj1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                Console.Write("Unesite cijeli broj: ");
+                int broj = int.Parse(Console.ReadLine());
+                if (broj % 3 == 0 || broj % 5 == 0)
+                {
+                Console.Write("Broj je dobar.");
+                }
+                else
+                {
+                Console.Write("Broj nije dobar.");
+                }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/5.2.7dodaj_broj2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2._7dodaj_broj2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite cijeli broj: ");
+            int broj = int.Parse(Console.ReadLine());
+            try
+            {
+                if (broj % 4 == 0 && broj >= 100)
+                {
+                    Console.Write("Broj je dobar.");
+                }
+                else
+                {
+                    Console.Write("Broj nije dobar.");
+                }
+                Console.ReadKey();
+            }catch(Exception e)
+            {
+                Console.Write("greška: " + e);
+            }
+        }
+    }
+}
