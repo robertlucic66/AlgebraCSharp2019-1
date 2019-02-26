ed65f9679c6efec1872af4a466966ed18e51c8c6
robertlucic66 <robertlucic66@outlook.com>
2019-02-26 18:12:01 +0100

    petlja for

---
+++ ConsoleApp1/5.3.1.1primjer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._3._1._1primjer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("{0}. ALGEBRA", (i + 1).ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/Petlje_primjeri/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petlje_primjeri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ispis svih brojeva od 0,1,2 ... 99");
+            //Ispis svih brojeva od 0,1,2 ... 99
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write("{0} ", i);
+            }
+
+            Console.WriteLine("");
+
+            Console.WriteLine("\n\nIspis svih brojeva od 16,18,20 ... 88");
+            //Ispis svih brojeva od 16,18,20 ... 88
+            for (int i = 16; i < 89; i+=2)
+            {
+                Console.Write("{0} ", i);
+            }
+
+            Console.WriteLine("\n\nIspis svih brojeva od 135,130,125 ... 55");
+            //Ispis svih brojeva od 16,18,20 ... 88
+            for (int i = 135; i > 54; i -= 5)
+            {
+                Console.Write("{0} ", i);
+            }
+
+            Console.WriteLine("Ispis svih brojeva od 0 do 99 osim 80");
+            //Ispis svih brojeva od 16,18,20 ... 88
+            for (int i = 0; i < 100; i++)
+            {
+                if (i == 80)
+                {
+                    continue;
+                }
+                Console.Write("{0} ", i);
+            }
+
+            Console.WriteLine("Ispis svih brojeva od 0 do 99 break na 80");
+            //Ispis svih brojeva od 16,18,20 ... 88
+            for (int i = 0; i < 100; i++)
+            {
+                if (i == 80)
+                {
+                    break;
+                }
+                Console.Write("{0} ", i);
+            }
+
+            Console.WriteLine("Ispis svih brojeva od 1 do 9999 koji su dijeljivi sa 17");
+            //Ispis svih brojeva od 16,18,20 ... 88
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 17 == 0)
+                {
+                    Console.Write("{0} ", i);
+                }
+                
+            }
+
+            Console.WriteLine("\n\nIspis svih brojeva od 135,130,125 ... 55");
+            //Ispis svih brojeva od 16,18,20 ... 88
+            for (int i = 135; i > 54; i -= 5)
+            {
+                Console.Write("{0} ", i);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
