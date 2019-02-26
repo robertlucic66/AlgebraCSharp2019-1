6be0ef6426f7e71378864f1ab4b6e8f0316032f0
Robert Lucić <robertlucic66@outlook.com>
2019-02-26 16:35:03 +0100

    u intervalu primjer

---
+++ ConsoleApp1/5.2.4.znamenkasti_broj/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2._4.znamenkasti_broj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite cijeli broj: ");
+            int a = int.Parse(Console.ReadLine());
+            if (a < 0)
+            {
+                a = -a;
+            }
+            if (a < 10)
+            {
+                Console.Write("Uneseni broj je jednoznamenkasti");
+            }else if(a<100){
+                Console.Write("Uneseni broj je dvoznamenkasti");
+            }else if (a < 1000)
+            {
+                Console.Write("Uneseni broj je troznamenkasti");
+            }
+            else
+            {
+                Console.Write("Uneseni broj je višeznamenkasti");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/5.2.5u_intervalu/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2._5u_intervalu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite cijeli broj: ");
+            int broj = int.Parse(Console.ReadLine());
+            if (broj >= 100 && broj <= 200)
+            {
+                Console.Write("Broj je u intervalu [100, 200]");
+            }
+            else
+            {
+                Console.Write("Broj nije u intervalu [100, 200]");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
