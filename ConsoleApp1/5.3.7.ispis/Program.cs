bfeb92e84023c77f40b52129bf8e0ea263ebb910
Robert Lucić <robertlucic66@outlook.com>
2019-03-02 20:54:10 +0100

    5.3.7

---
+++ ConsoleApp1/5.3.7.ispis/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._3._7.ispis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if ((i % 7 == 0) && ! (i % 3 == 0))
+                {
+                    Console.WriteLine(i.ToString());
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
