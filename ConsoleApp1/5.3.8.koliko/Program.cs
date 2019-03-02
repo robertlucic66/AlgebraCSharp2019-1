7d494031633ea3c617d0e31f968e97d554abae5f
Robert Lucić <robertlucic66@outlook.com>
2019-03-02 20:57:55 +0100

    5.3.8

---
+++ ConsoleApp1/5.3.8.koliko/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._3._8.koliko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int koliko = 0;
+            for(int i = 1; i <= 1000; i++)
+            {
+                if (i % 7 == 0 && !(i % 3 == 0))
+                {
+                    koliko++;
+                }
+            }
+            Console.WriteLine("Brojeva djeljivih sa 7, a nedjeljivih s 3 iz intervala [1, 1000] ima {0}", koliko);
+            Console.ReadLine();
+        }
+    }
+}
