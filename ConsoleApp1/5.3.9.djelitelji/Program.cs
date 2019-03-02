f9fbad9f18bfe93ca116f6fb85e1011de7b684ae
Robert Lucić <robertlucic66@outlook.com>
2019-03-02 21:04:23 +0100

    DZ -5.3.9

---
+++ ConsoleApp1/5.3.9.djelitelji/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._3._9.djelitelji
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite prirodan broj: ");
+            try
+            {
+                int broj = int.Parse(Console.ReadLine());
+                if (broj <= 0)
+                {
+                    throw new PrirodanBrojException("UNESITE PRIRODAN BROJ!");
+                }
+
+                Console.WriteLine("Djelitelji broja {0} su: ", broj);
+
+                for (int i = 1; i <= broj; i++)
+                {
+                    if (broj % i == 0)
+                    {
+                        Console.Write("{0}, ", i.ToString());
+                    }
+                }
+
+            }
+            catch (PrirodanBrojException ex)
+            {
+                Console.WriteLine("Greška. Poruka: " + ex.ToString());
+            }
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Greška. Poruka: " + ex.ToString());
+            }
+
+            finally
+            {
+                Console.ReadLine();
+            }
+    }
+}
+
+    public class PrirodanBrojException : Exception
+    {
+        public PrirodanBrojException()
+        {
+        }
+
+        public PrirodanBrojException(string message) : base(message)
+        {
+        }
+
+        public PrirodanBrojException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
