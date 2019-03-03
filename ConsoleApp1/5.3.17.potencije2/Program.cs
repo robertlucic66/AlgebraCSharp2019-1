e246851de9f4f27d3191d02fa93a6c133856e5aa
Robert Lucić <robertlucic66@outlook.com>
2019-03-03 14:49:40 +0100

    DZ-5.3.17

---
+++ ConsoleApp1/5.3.17.potencije2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._3._17.potencije2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 5.3.17 Potencije 2
+            // Napišite program koji ispisuje od 1. do 10.potencije broja 2.
+            int potencija;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                potencija = 1;
+
+                for (int j = 1; j <= i; j++)
+                {
+                    potencija *= 2;
+                }
+
+                Console.WriteLine("2 na {0} = {1}", i, potencija);
+            }
+            Console.ReadLine();
+        }
+    }
+}
