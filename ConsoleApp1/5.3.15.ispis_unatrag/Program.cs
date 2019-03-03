88d86bc0289250499c6e257a9726d2aec61b1e93
Robert Lucić <robertlucic66@outlook.com>
2019-03-03 14:46:33 +0100

    DZ-5.3.15

---
+++ ConsoleApp1/5.3.15.ispis_unatrag/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._3._15.ispis_unatrag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 5.3.15 Ispis unatrag
+            // Napišite program koji ispisuje prirodne brojeve iz intervala [1, 10] unatrag.
+            for (int i = 10; i >= 1; i--)
+            {
+                Console.WriteLine(i.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
