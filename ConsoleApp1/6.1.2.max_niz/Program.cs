00cde6637d762ba724a7518a5848c81c9c10a4f8
Robert Lucić <robertlucic66@outlook.com>
2019-03-04 18:38:51 +0100

    6.1.1. primjer

---
+++ ConsoleApp1/6.1.2.max_niz/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._1._2.max_niz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ ConsoleApp1/6.2.1.1.primjer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._2._1._1.primjer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int broj = -1;
+            ArrayList arr = new ArrayList();
+            Console.WriteLine("Unesi broj, za kraj unesi 0");
+
+            while (broj != 0)
+            {
+                broj = int.Parse(Console.ReadLine());
+                arr.Add(broj);
+            }
+            // ispis
+            for (int i = 0; i < arr.Count; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            foreach (int brojcic in arr)
+            {
+                Console.Write(" " + brojcic);
+            }
+            Console.ReadKey();
+        }
+    }
+}
