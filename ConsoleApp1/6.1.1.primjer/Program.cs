2769cc54501c0fdbc559ed883e62e1b28c7f167d
Robert Lucić <robertlucic66@outlook.com>
2019-03-04 18:05:51 +0100

    6.1.1 primjer

---
+++ ConsoleApp1/6.1.1.primjer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._1._1.primjer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // deklaracija
+            int[] niz = new int[3];
+
+            // init
+            niz[0] = 55;
+            niz[2] = 77;
+            niz[1] = 22;
+            try
+            {
+                for (int i = 0; i < niz.Length; i++)
+                {
+                    if (niz[i] == null)
+                    {
+                        throw new Exception("niz sadrži ne inicijaliziranu vrijednost na indexu " + i);
+                    }
+                    Console.WriteLine((i + 1) + ": " + niz[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
