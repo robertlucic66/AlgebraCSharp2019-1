cfd4ee5727b719392f442e71d9d7778b3cbfbf5e
Robert Lucić <robertlucic66@outlook.com>
2019-03-04 18:54:18 +0100

    6.1.2 Max

--- ConsoleApp1/6.1.2.max_niz/Program.cs
+++ ConsoleApp1/6.1.2.max_niz/Program.cs
@@ -10,6 +10,30 @@ namespace _6._1._2.max_niz
     {
         static void Main(string[] args)
         {
+            int broj;
+            int[] niz = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                broj = int.Parse(Console.ReadLine());
+                niz[i] = broj;                                               
+            }
+            for (int i = 0; i < niz.Length; i++)
+            {
+                if (niz[i] == nit.Max())
+                {
+                    ConsoleColor cc = Console.ForegroundColor; 
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(niz.Max());
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.Write(" " + niz[i]);
+                }
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
