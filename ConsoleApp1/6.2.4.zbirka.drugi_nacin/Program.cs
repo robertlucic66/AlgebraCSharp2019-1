21be7da981e268c1c701d113810f8e9f686b1f62
Robert Lucić <robertlucic66@outlook.com>
2019-03-05 18:16:20 +0100

    zavrseni nizovi i liste

---
+++ ConsoleApp1/6.2.4.zbirka.drugi_nacin/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._2._4.zbirka.drugi_nacin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ulaz_broj = Console.ReadLine();
+            int broj = int.Parse(ulaz_broj);
+            Console.WriteLine("Original: {0} obrnuti {1}", broj, new string(ulaz_broj.Reverse().ToArray()));
+            Console.ReadLine();
+        }
+    }
+}
--- ConsoleApp1/6.2.4.zbirka.znamenke/Program.cs
+++ ConsoleApp1/6.2.4.zbirka.znamenke/Program.cs
@@ -12,21 +12,52 @@ namespace _6._2._4.zbirka.znamenke
         static void Main(string[] args)
         {
             Console.Write("Unesite prirodan broj: ");
-            int broj = int.Parse(Console.ReadLine());
-
-            ArrayList arr = new ArrayList();
-            while (broj > 0)
+            try
             {
-                arr.Add(broj % 10);
-                broj /= 10;
+                int broj = int.Parse(Console.ReadLine());
+
+                ArrayList arr = new ArrayList();
+                while (broj > 0)
+                {
+                    arr.Add(broj % 10);
+                    broj /= 10;
+                }
+
+                Console.WriteLine("Ispis znamenki naopako: ");
+                for (int i = 0; i < arr.Count; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
             }
-
-            Console.WriteLine("Ispis znamenki naopako: ");
-            for ( int i = 0; i < arr.Count; i++)
+            catch(PrirodanBrojException pex)
             {
-                Console.WriteLine(arr[i]);
+                Console.WriteLine("Greška. Poruka: " + pex.ToString());
+            }
+            catch(FormatException fex)
+            {
+                Console.WriteLine("Greška. Poruka: " + fex.ToString());
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Greška. Poruka: " + ex.ToString());
             }
             Console.ReadLine();
         }
     }
 }
+
+    public class PrirodanBrojException : Exception
+    {
+        public PrirodanBrojException()
+        {
+        }
+
+        public PrirodanBrojException(string message) : base(message)
+        {
+        }
+
+        public PrirodanBrojException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+
