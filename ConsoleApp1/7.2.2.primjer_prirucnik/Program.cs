5fddc8ddb1253626de4c5928d5929bd4e8f6b581
Robert Lucić <robertlucic66@outlook.com>
2019-03-05 18:44:49 +0100

    primjer 7.2.2 prirucnik

---
+++ ConsoleApp1/7.2.2.primjer_prirucnik/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2._2.primjer_prirucnik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pozdrav("Robert");
+            Pozdrav("Robert", "Lucić");
+            Console.ReadKey();
+        }
+
+        static void Pozdrav(string ime)
+        {
+            Console.WriteLine("Dobar dan, " + ime + "!"); 
+        }
+
+        static void Pozdrav(string ime, string prezime)
+        {
+            Console.WriteLine("Dobar dan, " + ime + " " + prezime + "!");
+        }
+    }
+}
---
+++ ConsoleApp1/7.2.potprogrami/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2.potprogrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld();
+            Program.HelloWorld();
+            Console.WriteLine(HelloWorld("Ola"));
+            Console.ReadKey();
+
+
+        }
+        static void HelloWorld()
+        {
+            Console.WriteLine("Hello world");
+        }
+        static String HelloWorld(String poz)
+        {
+            return poz + " i tebi putniče";
+        }
+
+    }
+}
