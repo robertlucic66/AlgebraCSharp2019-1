05e2cc6324ab312986259f8499330d96ae46d058
Robert Lucić <robertlucic66@outlook.com>
2019-03-06 17:48:27 +0100

    objekti

---
+++ ConsoleApp1/8.1.2-prirucnik/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._1._2_prirucnik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instanciraj novi objekt klase Ucenik
+            Ucenik uc1 = new Ucenik();
+
+            // inicijalizacija objekta
+            Console.WriteLine("Unesi ime učenika");
+            uc1.ime = Console.ReadLine();
+
+            
+
+        }
+    }
+}
