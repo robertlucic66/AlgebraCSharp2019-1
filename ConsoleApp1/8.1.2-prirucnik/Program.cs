6d420f29893148fb124881a3f72cb330d8be43bb
Robert Lucić <robertlucic66@outlook.com>
2019-03-06 18:45:32 +0100

    objekti - kraj sata

--- ConsoleApp1/8.1.2-prirucnik/Program.cs
+++ ConsoleApp1/8.1.2-prirucnik/Program.cs
@@ -16,8 +16,34 @@ namespace _8._1._2_prirucnik
             // inicijalizacija objekta
             Console.WriteLine("Unesi ime učenika");
             uc1.ime = Console.ReadLine();
+            Console.WriteLine("Unesi prezime učenika: ");
+            uc1.prezime = Console.ReadLine();
+            Console.WriteLine("Unesi ocjenu iz matematike");
+            uc1.ocjenaIzMatematike = int.Parse(Console.ReadLine());
+            Console.WriteLine("Unesi ocjenu iz biologije");
+            uc1.ocjenaIzBiologije = int.Parse(Console.ReadLine());
+            Console.WriteLine("Unesi ocjenu iz engleskog");
+            uc1.ocjenaIzEngleskog = int.Parse(Console.ReadLine());
 
+            Console.WriteLine("Prosjek je: " + uc1.Prosjek());
             
+            
+            // instanciraj novi objekt klase Ucenik
+            Ucenik uc2 = new Ucenik("Perica");
+
+            Console.WriteLine("Ime učenika je: "+ uc1.ime+" i "+uc2.ime);
+
+            List<Ucenik> ucenici = new List<Ucenik> { uc1, uc2 };
+            ucenici.Add(uc1);
+            ucenici.Add(uc2);
+            //enici.Add()
+            foreach (var item in ucenici)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.ReadKey();
 
         }
     }
--- ConsoleApp1/8.1.2-prirucnik/Ucenik.cs
+++ ConsoleApp1/8.1.2-prirucnik/Ucenik.cs
@@ -1,7 +1,44 @@
-﻿namespace _8._1._2_prirucnik
+﻿using System;
+namespace _8._1._2_prirucnik
 {
     internal class Ucenik
     {
         public string ime = "";
+        public DateTime datumNastanka;
+        public string prezime = "";
+        public int ocjenaIzMatematike = 0;
+        public int ocjenaIzEngleskog = 0;
+        public int ocjenaIzBiologije = 0;
+        private string v;
+
+        public Ucenik()
+        {
+            this.datumNastanka = DateTime.Now;
+        }
+
+        public Ucenik(string v)
+        {
+            this.v = v;
+        }
+
+        public Ucenik(string v, string prez)
+        {
+            this.ime = v;
+            this.prezime = prez;
+        }
+
+        internal double Prosjek()
+        {
+            double prosjek = 0;
+            prosjek = (ocjenaIzBiologije+ ocjenaIzEngleskog + ocjenaIzMatematike) / 3;
+
+            return prosjek;
+
+            //throw new NotImplementedException();
+        }
+        public override string ToString()
+        {
+            return "Moje ime je " + this.ime + " a moj prosjek je: " + this.Prosjek();
+        }
     }
 }
