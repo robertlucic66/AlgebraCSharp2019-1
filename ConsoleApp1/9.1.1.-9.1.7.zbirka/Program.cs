f4c8aca75fbaeba886f8a6a6f5d7655274b73c38
Robert Lucić <robertlucic66@outlook.com>
2019-03-13 18:48:08 +0100

    rad s tekstom

---
+++ ConsoleApp1/9.1.1.-9.1.7.zbirka/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._1._1._9._1._7.zbirka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("9.1.1. znak u rijeci");
+            string rijec = "Danas je suncan dan";
+            char slovo = 'n';
+
+            int brojac = 0;
+            for (int i = 0; i < rijec.Length; i++)
+            {
+                if(rijec[i] == slovo)
+                {
+                    brojac++;
+                }
+            }
+            Console.WriteLine("Znak {0} pojavljuje se u rijeci '{1}' {2} puta", slovo, rijec, brojac);
+            Console.ReadKey();
+        }
+    }
+}
