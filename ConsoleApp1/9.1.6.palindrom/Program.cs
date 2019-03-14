aa18b10df5c73aab6e190094626cb99008aad7cf
Robert Lucić <robertlucic66@outlook.com>
2019-03-14 12:06:31 +0100

    palindrom

---
+++ ConsoleApp1/9.1.6.palindrom/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._1._6.palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite liniju teksta: ");
+            string tekst = Console.ReadLine();
+
+            tekst = tekst.Replace(" ", "");
+
+            string obrnuto = "";
+            for(int i = tekst.Length -1; i > 0; i--)
+            {
+                obrnuto += tekst[i];
+            }
+
+            if (tekst == obrnuto)
+            {
+                Console.WriteLine("To je palindrom!");
+            }
+            else
+            {
+                Console.WriteLine("To nije palindrom!");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
